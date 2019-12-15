d468854aa1bd2f1d54b50a5c037fc7a50e251bf6
Ruslan Samulyak <ruslan.samulyak@gmail.com>
2019-12-15 18:31:00 +0200

    Incorrect login error message in multiple places

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -54,7 +54,7 @@ class RegisterController extends Controller
         return Validator::make($data, [
             'name' => ['required', 'string', 'max:255'],
             'nation' => ['required', 'string', 'max:255'],
-            'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
+            'reg_email' => ['required', 'string', 'email', 'max:255', 'unique:users,email'],
             'password' => ['required', 'string', 'min:8', 'confirmed'],
         ]);
     }
@@ -69,7 +69,7 @@ class RegisterController extends Controller
     {        
         $user = new User([
             'name' => $data['name'],
-            'email' => $data['email'],
+            'email' => $data['reg_email'],
             'password' => Hash::make($data['password'])
         ]);
 
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -232,7 +232,7 @@
                             <label for="nation" class="col-md-4 col-form-label text-md-right">{{ __('Nation') }}</label>
 
                             <div class="col-md-6">
-                                <div id="NationSearch"></div>
+                                <div id="NationSearch" value="{{ old('nation') }}"></div>
                                 {{-- <input id="nation" type="text" class="form-control @error('nation') is-invalid @enderror" name="nation" value="{{ old('nation') }}" required autocomplete="nation" autofocus> --}}
 
                                 @error('nation')
@@ -247,9 +247,9 @@
                             <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
 
                             <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
+                                <input id="email" type="email" class="form-control @error('reg_email') is-invalid @enderror" name="reg_email" value="{{ old('reg_email') }}" required autocomplete="email">
 
-                                @error('email')
+                                @error('reg_email')
                                     <span class="invalid-feedback" role="alert">
                                         <strong>{{ $message }}</strong>
                                     </span>
