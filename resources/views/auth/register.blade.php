77b6388dc13a25604ad2786b848091ce86c07cd7
Ruslan Samulyak <ruslan.samulyak@gmail.com>
2019-12-17 22:37:29 +0200

    Register page update

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -51,7 +51,7 @@
                             </div>
                         </div>
                         <div class="row">
-                            <div class="form-group col-md-12 row">
+                            <div class="form-group col-md-6 row">
                                 <div class="col-md-6 offset-md-4">
                                     <div class="form-check">
                                         <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
@@ -62,17 +62,22 @@
                                     </div>
                                 </div>
                             </div>
-                            <div class="form-group col-md-12 row mb-0">
+                            
+                            <div class="form-group col-md-6 row mb-0 mr-0">
                                 <div class="col-md-8 offset-md-4">
-                                    <button type="submit" class="btn btn-primary">
-                                        {{ __('Login') }}
-                                    </button>
-
                                     @if (Route::has('password.request'))
-                                        <a class="btn btn-link" href="{{ route('password.request') }}">
+                                        <a href="{{ route('password.request') }}">
                                             {{ __('Forgot Your Password?') }}
                                         </a>
                                     @endif
+                                </div>
+                            </div>
+                            
+                            <div class="form-group col-md-12 row mb-0">
+                                <div class="col-md-12 text-center">
+                                    <button type="submit" class="btn btn-primary col-md-2">
+                                        {{ __('Login') }}
+                                    </button>
                                 </div>
                             </div>
                         </div>
@@ -88,13 +93,18 @@
     <div class="row justify-content-center">
         <div class="col-md-12">
             <div class="card">
-                <div class="card-header">{{ __('Register') }}</div>
+                <div class="card-header">{{ __('Registration') }}</div>
 
-                <div class="card-body">
-                    <h4>Egora User Agreement</h4>
-                    <p>By this document, <u>&nbsp; &nbsp; <span id="regName">(your name)</span> &nbsp; &nbsp;</u> - hence forth referred to as the "user" 
-                    - and the International Logic Party - the citizens and proprietors of the Republic of Egora (a.k.a. Egora), represented by the 
-                    administration of Egora - enter into and agreement on the following terms.</p>
+                <div class="card-body row">
+                    <div class="col-md-12">
+                        <div class="text-center">
+                            <h4>Egora User Agreement</h4>
+                        </div>
+                        <p>By this document, <u>&nbsp; &nbsp; <span id="regName">(your name)</span> &nbsp; &nbsp;</u> - hence forth referred to as the "user" 
+                        - and the International Logic Party - the citizens and proprietors of the Republic of Egora (a.k.a. Egora), represented by the 
+                        administration of Egora - enter into and agreement on the following terms.</p>
+                    </div>
+                    <div class="offset-1 col-md-10 pl-5 pr-5">
                     <ol>
                         <li>
                             <span class="summary">
@@ -200,9 +210,10 @@
                             </div>
                         </li>                        
                     </ol>
+                    </div>
                 </div>
                 <div class="card-body">
-                    So agreed on <u> {{ (new \Carbon\Carbon())->format('d.m.Y') }} </u>
+                    So agreed on <u> {{ (new \Carbon\Carbon())->format('d/m/Y') }} </u>
                    <div class="row">
                        <div class="col-md-3 offset-md-2"><u>&nbsp; &nbsp; <span id="regName2">(your name)</span> &nbsp; &nbsp;</u></div>
                        <div class="col-md-2">and</div>
@@ -210,14 +221,15 @@
                    </div>
                </div>
                 
-                <div class="card-body">
-                    <form id="register" method="POST" action="{{ route('register') }}">
+                <form id="register" method="POST" action="{{ route('register') }}">
+                    <div class="card-body">
                         @csrf
 
-                        <div class="form-group row">
-                            <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('Your Name') }}</label>
+                        <div class="row">
+                            <div class="form-group offset-1 col-md-10 row">
+                            <label for="name" class="col-md-3 col-form-label">{{ __('Your Name') }}</label>
 
-                            <div class="col-md-6">
+                            <div class="col-md-8">
                                 <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name">
 
                                 @error('name')
@@ -226,12 +238,14 @@
                                     </span>
                                 @enderror
                             </div>
+                            </div>
                         </div>
 
                         <div class="form-group row">
-                            <label for="nation" class="col-md-4 col-form-label text-md-right">{{ __('Nation') }}</label>
+                            <div class="form-group offset-1 col-md-10 row">
+                            <label for="nation" class="col-md-3 col-form-label">{{ __('Nation') }}</label>
 
-                            <div class="col-md-6">
+                            <div class="col-md-8">
                                 <div id="NationSearch" value="{{ old('nation') }}"></div>
 
                                 @error('nation')
@@ -240,12 +254,14 @@
                                     </span>
                                 @enderror
                             </div>
+                            </div>
                         </div>
 
                         <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
+                            <div class="form-group offset-1 col-md-10 row">
+                            <label for="email" class="col-md-3 col-form-label">{{ __('E-Mail Address') }}</label>
 
-                            <div class="col-md-6">
+                            <div class="col-md-8">
                                 <input id="email" type="email" class="form-control @error('reg_email') is-invalid @enderror" name="reg_email" value="{{ old('reg_email') }}" required autocomplete="email">
 
                                 @error('reg_email')
@@ -254,12 +270,14 @@
                                     </span>
                                 @enderror
                             </div>
+                            </div>
                         </div>
 
                         <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
+                            <div class="form-group offset-1 col-md-10 row">
+                            <label for="password" class="col-md-3 col-form-label">{{ __('Create Password') }}</label>
 
-                            <div class="col-md-6">
+                            <div class="col-md-8">
                                 <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
 
                                 @error('password')
@@ -268,25 +286,31 @@
                                     </span>
                                 @enderror
                             </div>
+                            </div>
                         </div>
 
                         <div class="form-group row">
-                            <label for="password-confirm" class="col-md-4 col-form-label text-md-right">{{ __('Confirm Password') }}</label>
+                            <div class="form-group offset-1 col-md-10 row">
+                            <label for="password-confirm" class="col-md-3 col-form-label">{{ __('Confirm Password') }}</label>
 
-                            <div class="col-md-6">
+                            <div class="col-md-8">
                                 <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
+                            </div>
                             </div>
                         </div>
 
-                        <div class="form-group row mb-0">
-                            <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
+                        <div class="row">
+                            <div class="form-group col-md-12 row mb-0">
+                            <div class="col-md-12 text-center">
+                                <button type="submit" class="btn btn-primary col-md-2">
                                     {{ __('Register') }}
                                 </button>
                             </div>
+
+                            </div>
                         </div>
-                    </form>
-                </div>
+                    </div>
+                </form>
             </div>
         </div>
     </div>
