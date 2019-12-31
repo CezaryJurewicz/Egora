a8f6ccdf93859579f9f63bd9e5325d99c89420fa
Anton Matiyenko <amatiyenko@gmail.com>
2019-12-31 19:01:29 +0200

    Case-sensitive Nation

---
+++ database/migrations/2019_12_31_110056_nation_collation.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class NationCollation extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up() {
+        DB::statement("ALTER TABLE nation CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_bin");
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
