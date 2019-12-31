74a967ac4034b5ddef48f179fbf97b7b573a736b
Anton Matiyenko <amatiyenko@gmail.com>
2019-12-31 18:49:07 +0200

    Case-sensitive Nation

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -73,7 +73,7 @@ class RegisterController extends Controller
             'password' => Hash::make($data['password'])
         ]);
 
-        $nation = Nation::where('title', $data['nation'])->first();
+        $nation = Nation::where('title', 'LIKE', $data['nation'])->first();
                 
         if (is_null($nation)) {
             $nation = Nation::create([
