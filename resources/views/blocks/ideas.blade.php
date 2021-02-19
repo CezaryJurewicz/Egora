878764f219961f445aa0bc231f08c061be3bcfe1
Ruslan Samulyak <ruslan.samulyak@gmail.com>
2021-02-20 01:02:21 +0200

    Indexes idea header layout

--- resources/views/blocks/ideas.blade.php
+++ resources/views/blocks/ideas.blade.php
@@ -4,27 +4,35 @@
                     <div class="mb-3">
                         <div class="p-2">
                             <div class="row">
-                                <div class="col-2">#{{$i + $ideas->firstItem()}} </div>
-                                @if (is_egora())
-                                    <div class="col-7 col-sm-4">{{$idea->nation->title}} </div>
-                                @elseif (is_egora('community'))
-                                    <div class="col-7 col-sm-4">{{$idea->community->title}} </div>
-                                @endif
+                                <div class="col-8 col-sm-5">
+                                    <div class="row">
+                                        <div class="col-12">#{{$i + $ideas->firstItem()}} </div>
+                                    </div>
+                                    <div class="row">
+                                        <div class="col-12">
+                                        @if (is_egora())
+                                            {{$idea->nation->title}}
+                                        @elseif (is_egora('community'))
+                                            {{$idea->community->title}}
+                                        @endif
+                                        </div>
+                                    </div>
+                                </div>
                                 <div class="col-2 text-center">
                                     @if ((auth('web')->user()?:auth('admin')->user())->can('view', $idea))
                                     <a class="btn btn-sm btn-primary" href="{{ route('ideas.view', $idea->id) }}">{{ __('Open') }}</a>
                                     @endif
                                 </div>
-                                <div class="col-12 col-sm-4" style1="padding: 0;">
+                                <div class="offset-md-1 col-12 col-sm-4 pr-0">
                                     <div class="row">
-                                        <div class="col-6">
+                                        <div class="col-7 col-sm-6">
                                         @if($index == 'dominance')
                                         IDI Points: 
                                         @else
                                         Supporters:
                                         @endif
                                         </div>
-                                        <div class="col-6">
+                                        <div class="col-5">
                                         @if($index == 'dominance')
                                         {{ number_format($idea->liked_users_sum) }}
                                         @else
@@ -33,14 +41,14 @@
                                         </div>
                                     </div>
                                     <div class="row">
-                                        <div class="col-6">
+                                        <div class="col-7 col-sm-6">
                                         @if($index == 'dominance')
                                         Supporters:
                                         @elseif (is_egora())
                                         IDI Points: 
                                         @endif
                                         </div>
-                                        <div class="col-6">
+                                        <div class="col-5">
                                         @if($index == 'dominance')
                                         {{ number_format($idea->liked_users_count) }}
                                         @elseif (is_egora())
