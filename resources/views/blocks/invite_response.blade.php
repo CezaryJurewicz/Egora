3f714c115427abc6588064539f1c187fa9f898d4
Ruslan Samulyak <ruslan.samulyak@gmail.com>
2021-02-28 03:59:51 +0200

    invite response fix

--- resources/views/blocks/invite_response.blade.php
+++ resources/views/blocks/invite_response.blade.php
@@ -1,6 +1,6 @@
                     @if( Auth::guard('web')->check() && Auth::guard('web')->user()->can('invite_response', $notification) )
                     
-                        @if(Auth::guard('web')->check() && Auth::guard('web')->user()->user_notifications_new()->first(function ($v, $k) use ($notification) {
+                        @if(Auth::guard('web')->check() && Auth::guard('web')->user()->user_notifications_new->first(function ($v, $k) use ($notification) {
                             return $v->pivot->sender_id == Auth::guard('web')->user()->id && $v->pivot->notification_id == $notification->id;
                         }))
                             Response sent.                            
