92c2463755328ac4876480e94f810e5c5a9e6d38
Ruslan Samulyak <ruslan.samulyak@gmail.com>
2021-08-22 19:14:19 +0300

    Ui

--- resources/views/users/search.blade.php
+++ resources/views/users/search.blade.php
@@ -40,21 +40,21 @@
                         </div>
                     </div>
                     <div class="form-group row">
-                        <label for="followers" class="col-md-3 col-form-label">{{ __('My followers') }}</label>
+                        <label for="followers" class="col-md-4 col-form-label">{{ __('My followers') }}</label>
 
                         <div class="col-md-1">
                             <input class="form-control-sm" id="followers" name="followers" value=1 type="checkbox" {{ (old('followers')?: $followers) ? ' checked' : '' }}>
                         </div>
                     </div>
                     <div class="form-group row">
-                        <label for="officer" class="col-md-3 col-form-label">{{ __('Search ILP officers') }}</label>
+                        <label for="officer" class="col-md-4 col-form-label">{{ __('Search ILP officers') }}</label>
 
                         <div class="col-md-1">
                             <input class="form-control-sm" id="officer" name="officer" value=1 type="checkbox" onClick="c = document.getElementById('officer_petitioner'); c.checked = false;" {{ (old('officer')?: $officer) ? ' checked' : '' }}>
                         </div>
                     </div>
                     <div class="form-group row">
-                        <label for="officer_petitioner" class="col-md-3 col-form-label">{{ __('Search officer-petitioners') }}</label>
+                        <label for="officer_petitioner" class="col-md-4 col-form-label">{{ __('Search officer-petitioners') }}</label>
 
                         <div class="col-md-1">
                             <input class="form-control-sm" id="officer_petitioner" name="officer_petitioner" value=1 type="checkbox" onClick="c = document.getElementById('officer'); c.checked = false;" {{ (old('officer_petitioner')?: $officer_petitioner) ? ' checked' : '' }} >
