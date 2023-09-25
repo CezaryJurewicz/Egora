1947f2c743f6f9b93517c922771dede1de9e4bf5
Ruslan Samulyak <ruslan.samulyak@gmail.com>
2023-09-26 01:07:36 +0300

    update php/js libs

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,6 +11,7 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.react('resources/js/app.js', 'public/js')
+mix.js('resources/js/app.js', 'public/js')
+    .react()
    .sass('resources/sass/app.scss', 'public/css')
    .version();
