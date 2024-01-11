8191d9114ef53217e5885941086cbf388c7fb5dd
Ruslan Samulyak <ruslan.samulyak@gmail.com>
2024-01-12 01:26:14 +0200

    external url

--- app/Http/helpers.php
+++ app/Http/helpers.php
@@ -312,7 +312,7 @@ function _url_search_name($search_name) {
 }
 
 function _url_replace($url, $clear=false) {
-    $result = str_replace(['www.', 'test.', 'http://', 'https://', 'egora','ilp'], ['', '', 'Egora', 'ILP'], $url);
+    $result = str_replace(['www.', 'test.', 'http://', 'https://', 'egora','ilp'], ['', '', '', '', 'Egora', 'ILP'], $url);
     
     if ($clear) {
         $result = substr_replace($result, '/ ', strrpos($result, '/'));
