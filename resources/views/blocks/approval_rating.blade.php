94706e68a2839f03c5e1e01fd97712a617e15550
Ruslan Samulyak <ruslan.samulyak@gmail.com>
2024-03-25 01:07:09 +0200

    Safari bug fix

--- resources/views/blocks/approval_rating.blade.php
+++ resources/views/blocks/approval_rating.blade.php
@@ -3,7 +3,7 @@
                 <h5>@lang('Approval Rating')</h5>
             </div>
             <div class="card-body text-center">
-                <div class="row">
+                <div class="row" style="height: 300px;">
                     <div class="col">
                         <span class="col p-0" id="ApprovalRating" value="{{ $idea->id }}"></span>
                     </div>
